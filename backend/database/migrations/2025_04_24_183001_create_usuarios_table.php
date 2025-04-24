849350a5fa3d30ea6068fdddb2c3d9989dbdbe30
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-04-24 20:33:16 +0200

    migraciones para usuarios

---
+++ backend/database/migrations/2025_04_24_183001_create_usuarios_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nombre', 100);
+            $table->string('apellidos', 150);
+            $table->string('dni_usuario', 9)->unique();
+            $table->string('email', 100)->unique();
+            $table->date('fecha_nacimiento');
+            $table->string('telefono', 15)->nullable();
+            $table->enum('rol', ['paciente', 'especialista', 'usuario', 'administrador'])->default('usuario');
+            $table->date('fecha_creacion');
+            $table->date('fecha_actualizacion')->nullable();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('usuarios');
+    }
+};
