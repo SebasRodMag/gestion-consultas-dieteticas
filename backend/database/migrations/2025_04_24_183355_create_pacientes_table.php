e40efbc3050cceaa883be50c8e75b63144d1d87f
Sebastián Rodríguez <153898337+SebasRodMag@users.noreply.github.com>
2025-04-24 20:34:37 +0200

    migraciones pacientes

---
+++ backend/database/migrations/2025_04_24_183355_create_pacientes_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id('id_paciente');
+            $table->unsignedBigInteger('id_usuario')->unique();
+            $table->date('fecha_alta');
+            $table->date('fecha_baja')->nullable();
+
+            $table->foreign('id_usuario')->references('id_usuario')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('pacientes');
+    }
+};
